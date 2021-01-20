c136aa699bc1f8228b9e8856ceb5fbedd6962a8b
Katy La Tour <klatour324@gmail.com>
2021-01-20 10:22:30 -0600

    Add check_stock and stock methods to Vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -6,4 +6,20 @@ class Vendor
     @name = name
     @inventory = {}
   end
+
+  def check_stock(item)
+    if @inventory[item]
+      @inventory[item]
+    else
+      0
+    end
+  end
+
+  def stock(item, quantity)
+    if check_stock(item)  == 0
+      @inventory[item] = quantity
+    else
+      @inventory[item] += quantity
+    end
+  end
 end
