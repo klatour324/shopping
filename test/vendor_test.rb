1ca716d72720b7026ef8ee1f10b35f146e748779
Katy La Tour <klatour324@gmail.com>
2021-01-20 10:22:05 -0600

    Create test to check stock of inventory

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -11,45 +11,25 @@ class VendorTest < Minitest::Test
     assert_equal "Rocky Mountain Fresh", vendor.name
     assert_equal ({}), vendor.inventory
   end
-end
 
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# pry(main)> item2.name
-# #=> "Tomato"
-#
-# pry(main)> item2.price
-# #=> 0.5
-#
-# #=> #<Vendor:0x00007f85683152f0...>
-#
-# pry(main)> vendor.name
-# #=> "Rocky Mountain Fresh"
-#
-# pry(main)> vendor.inventory
-# #=> {}
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 0
-#
-# pry(main)> vendor.stock(item1, 30)
-#
-# pry(main)> vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 30
-#
-# pry(main)> vendor.stock(item1, 25)
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 55
-#
-# pry(main)> vendor.stock(item2, 12)
-#
-# pry(main)> vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
-# ```
+  def test_it_can_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    assert_equal 0, vendor.check_stock(item1)
+
+    vendor.stock(item1, 30)
+
+    assert_equal ({item1 => 30}), vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+
+    vendor.stock(item1, 25)
+
+    assert_equal 55, vendor.check_stock(item1)
+
+    vendor.stock(item2, 12)
+
+    assert_equal ({item1 => 55, item2 => 12}), vendor.inventory
+  end
+end
