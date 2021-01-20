cc831828ab97005e26f1cde064ca653c9811b1e8
Katy La Tour <klatour324@gmail.com>
2021-01-20 11:55:33 -0600

    Create test for date and sell

--- test/market_test.rb
+++ test/market_test.rb
@@ -153,4 +153,39 @@ class MarketTest < Minitest::Test
 
     assert_equal [item1], market.overstocked_items
   end
+
+  def test_sell
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item5 = Item.new({name: 'Onion', price: '$0.25'})
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+
+
+   refute market.sell(item1, 200)
+   refute market.sell(item5, 1)
+
+   assert market.sell(item4, 5)
+   assert_equal 45, vendor2.check_stock(item4)
+
+   assert market.sell(item1, 40)
+   assert_equal 0, vendor1.check_stock(item1)
+   assert_equal 60, vendor3.check_stock(item1)
+
+   assert_equal Date.today, market.date
+  end
 end
