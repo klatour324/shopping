c0df1e75eba0b8e32a348c4ddb55da8576fb6723
Katy La Tour <klatour324@gmail.com>
2021-01-20 10:29:53 -0600

    Create test for Market existence with attributes

---
+++ test/market_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+  def test_it_exists_with_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+
+    assert_instance_of Market, market
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+end
